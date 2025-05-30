1e16300ecce39d831f4e88278ff26bd9dacf2119
Soma Dinesh <somadineshboddu2004@gmail.com>
2025-05-30 14:35:07 +0530

    ep-9

--- my_project/app/Models/Job.php
+++ my_project/app/Models/Job.php
@@ -2,35 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Foundation\Console\AboutCommand;
-use Illuminate\Support\Arr;
-class Job{
-    public static function all():array        
-    {
-        return [
-        [
-            'id'=>1,
-            'title'=>'Director',
-            'salary'=>'$50,000'
-        ],
-        [
-            'id'=>2,
-            'title'=>'Manager',
-            'salary'=>'$40,000'
-        ],
-        [
-            'id'=>3,
-            'title'=>'Developer',
-            'salary'=>'$30,000'
-        ],
-    ] ;
-    }
-    public static function find(int $id){
-        $job= Arr::first(static::all(),fn($job) => $job['id'] == $id);
+use Illuminate\Database\Eloquent\Model;
 
-        if(!$job){
-            abort(404);
-        }
-    }
+class Job extends Model{
+    protected $table='job_listing';
+    protected $fillable=['title','salary'];
 }
 ?>
--- my_project/routes/web.php
+++ my_project/routes/web.php
@@ -6,7 +6,10 @@ use App\Models\Job;
 
 
 Route::get('/', function () {
+    
+    
     return view('home');
+
 });
 Route::get('/jobs', function ()  {
      return view('jobs',['jobs'=>Job::all()]);
