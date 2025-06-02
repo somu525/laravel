d2b7a71a6d076db1206d693f276f16f19a839b6e
Soma Dinesh <somadineshboddu2004@gmail.com>
2025-06-02 10:59:21 +0530

    ep-10

--- my_project/app/Models/Employer.php
+++ my_project/app/Models/Employer.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-class Job extends Model{
+class Employer extends Model
+{
+    /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
-    protected $table='job_listing';
-    protected $fillable=['title','salary'];
 
    
 }
-?>
