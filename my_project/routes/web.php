c20b1b10bcb88ecb4a299d94e012fd5a9d0045d0
Soma Dinesh <somadineshboddu2004@gmail.com>
2025-06-03 12:37:31 +0530

    ep-13

--- my_project/routes/web.php
+++ my_project/routes/web.php
@@ -12,7 +12,8 @@ Route::get('/', function () {
 
 });
 Route::get('/jobs', function ()  {
-     return view('jobs',['jobs'=>Job::all()]);
+    $jobs= Job::with('employer')->get(); 
+    return view('jobs',['jobs'=>$jobs]);
 });
 Route::get('/jobs/{id}', function ($id) {
     $job=Job::find($id);
