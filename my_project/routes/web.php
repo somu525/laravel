21908d9deb4b8d4b3d0d5de8dacab51cd834b8d4
Soma Dinesh <somadineshboddu2004@gmail.com>
2025-06-04 17:43:48 +0530

    ep-17

--- my_project/routes/web.php
+++ my_project/routes/web.php
@@ -22,7 +22,13 @@ Route::get('/jobs/{id}', function ($id) {
     $job=Job::find($id);
     return view('jobs.show',['job'=>$job]);
 });
+
 Route::post('/jobs',function() {
+    request()->validate([
+        'title'=>['required','min:3'],
+        'salary'=>['required']
+
+    ]);
     Job::create([
         'title'=>request('title'),
         'salary'=>request('salary'),
