74e2fbc8925934c5bf1587cda6ce83e4c0d045cd
Soma Dinesh <somadineshboddu2004@gmail.com>
2025-06-05 11:37:10 +0530

    ep-18

--- my_project/routes/web.php
+++ my_project/routes/web.php
@@ -4,25 +4,25 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-
+//home
 Route::get('/', function () {
-    
-    
     return view('home');
-
 });
+//index
 Route::get('/jobs', function ()  {
     $jobs= Job::with('employer')->latest()->simplePaginate(3); 
     return view('jobs.index',['jobs'=>$jobs]);
 });
+//create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
+//show
 Route::get('/jobs/{id}', function ($id) {
     $job=Job::find($id);
     return view('jobs.show',['job'=>$job]);
 });
-
+//store
 Route::post('/jobs',function() {
     request()->validate([
         'title'=>['required','min:3'],
@@ -36,6 +36,32 @@ Route::post('/jobs',function() {
     ]);
     return redirect('/jobs');
 });
+//edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job=Job::find($id);
+    return view('jobs.edit',['job'=>$job]);
+});
+//update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title'=>['required','min:3'],
+        'salary'=>['required']
+
+    ]);
+    $job=Job::findOrFail($id);
+    $job->update([
+        'title'=>request('title'),
+        'salary'=>request('salary')
+    ]);
+    return redirect('/jobs/'.$job->id);
+});
+//delete
+Route::delete('/jobs/{id}', function ($id) {
+    $job=Job::findOrFail($id);
+    $job->delete();
+    return redirect('/jobs');
+});
+//contact
 Route::get('/contact', function () {
     return view('contact');
 });
